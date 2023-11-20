899a9f45714721c94cd4acdbcbed8fb26c4c2772
Luigi Niso <luiginiso04@gmail.com>
2023-11-20 12:48:22 +0100

    Creating Project

---
+++ matrix_multiplication/matrix_multiplication/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  matrix_multiplication
+//
+//  Created by Luigi Niso on 20/11/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
